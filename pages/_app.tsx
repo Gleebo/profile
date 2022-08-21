88778ca35f2e2ced305e2960f8f1b0d3473bc981
Gleb <gleb.tsoy754@gmail.com>
2022-08-21 19:18:55 +1000

    Add icons styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '@fortawesome/fontawesome-svg-core'
+import '../fontello/css/fontello.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
