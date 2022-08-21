dada3ba3489957e40d0164b949652bf0d9a49633
Gleb <gleb.tsoy754@gmail.com>
2022-08-21 22:03:51 +1000

    Change section style

--- components/section.tsx
+++ components/section.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 export default function Section({id, title, children} : Props) {
   return (
-    <section id={id}>
-      <header className="bg-yellow-100 text-slate-800 rounded h-fit">
+    <section id={id} className="mb-12 [&:last-child]:mb-0 scroll-mt-40">
+      <header className="bg-yellow-100 text-slate-800 h-fit rounded-lg">
         <h1 className="text-base sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl p-1">
           <span
             className="text-3xl border-r px-1 border-slate-800 mr-1
@@ -20,7 +20,7 @@ export default function Section({id, title, children} : Props) {
           <span>{title.substring(2)}</span>
         </h1>
       </header>
-      <main className="mt-1">
+      <main className="min-h-[50vh] pt-8 pb-2 px-2 md:px-4 border-yellow-100">
         {children}
       </main>
     </section>
