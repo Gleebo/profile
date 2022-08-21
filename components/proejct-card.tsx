2c7d9f097b696fa3430d15f726a40cf53f6c0402
Gleb <gleb.tsoy754@gmail.com>
2022-08-21 22:03:27 +1000

    Change project card style

--- components/proejct-card.tsx
+++ components/proejct-card.tsx
@@ -18,7 +18,7 @@ export default function ProjectCard({ project, children }: Props) {
   return (
     <div className="border-2 border-yellow-100 rounded">
       <header>
-        <h1 className="text-lg md:text-xl lg:text-2xl xl:text-3xl border-b border-yellow-100 p-2 flex justify-between">
+        <h2 className="text-lg md:text-xl lg:text-2xl xl:text-3xl border-b border-yellow-100 p-2 flex justify-between">
           <a href={project.link} className="hover:underline">
             <span>{project.title}</span>
             {project.link && <FontAwesomeIcon icon={faLink} />}
@@ -29,8 +29,8 @@ export default function ProjectCard({ project, children }: Props) {
               <FontAwesomeIcon icon={faLink} />
             </a>
           )}
-        </h1>
-        <h2 className="flex p-2 border-b border-yellow-100 text-sm md:text-base lg:text-lg xl:text-xl">
+        </h2>
+        <h3 className="flex p-2 border-b border-yellow-100 text-sm md:text-base lg:text-lg xl:text-xl">
           {project.stack.map((tech) => (
             <span
               className="bg-yellow-100 text-slate-800 rounded-r-full rounded-l-full px-2 py-1 mr-1"
@@ -39,7 +39,7 @@ export default function ProjectCard({ project, children }: Props) {
               {tech}
             </span>
           ))}
-        </h2>
+        </h3>
       </header>
       <main>
         <p className="md:text-lg lg:text-xl xl:text-2xl p-2">
