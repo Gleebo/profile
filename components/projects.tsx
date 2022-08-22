a887a08840f86de4feb47bd1955823c069db91a8
Gleb <gleb.tsoy754@gmail.com>
2022-08-22 18:51:35 +1000

    Add a project

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,16 +5,23 @@ const projects = [
     title: "Web Profile",
     repoLink: "https://github.com/Gleebo/profile",
     stack: ["TypeScript", "React", "Next", "Tailwind"],
-    description: 'This web page! I made it with TypeScript, NextJS, ReactJS and TailwindCSS. It is sort of an online CV/Portfolio.'
+    description:
+      "This web page! I made it with TypeScript, NextJS, ReactJS and TailwindCSS. It is sort of an online CV/Portfolio.",
+  },
+  {
+    title: "Battle Gems",
+    repoLink: "https://github.com/Gleebo/terminal-app",
+    stack: ["Ruby"],
+    description: "A fun little project. Terminal based Turn Based RPG that I made to improve my OOP understanding. Written in Ruby."
   },
 ];
 
 export default function Projects() {
   return (
-    <>
+    <div className="space-y-6">
       {projects.map((project) => (
-        <ProjectCard project={project} key={project.title}></ProjectCard>
+        <ProjectCard project={project} key={project.title} />
       ))}
-    </>
+    </div>
   );
 }
