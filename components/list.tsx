0a102257a8561dacaa8182ed72850dfd75589b36
Gleb <gleb.tsoy754@gmail.com>
2022-08-27 12:55:29 +1000

    Add max width to List component

--- components/list.tsx
+++ components/list.tsx
@@ -1,6 +1,6 @@
 export default function List({ children }: { children: React.ReactNode }) {
   return (
-    <ul className="list-disc list-outside pl-8 pr-2 text-base md:text-lg lg:text-xl xl:text-2xl">
+    <ul className="list-disc list-outside pl-8 pr-2 text-base md:text-lg lg:text-xl xl:text-2xl space-y-2 max-w-[75ch]">
       {children}
     </ul>
   );
